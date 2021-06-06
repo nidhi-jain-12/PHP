a14637746a639d6171456c30f06340bb068dc806
nidhi-jain-12 <85149221+nidhi-jain-12@users.noreply.github.com>
2021-06-06 14:22:12 +0530

    Update form.php

--- day_3/form.php
+++ day_3/form.php
@@ -1,49 +1,48 @@
 <html>
-<head>
-    <title>Registration Form</title>
-<body>
+	<body>
 <!---
 GET: Not secure
 POST: secure
-Action: You can specify a page name where data will submit.
+Acion: You can specify a page name where data will submit.
 -->
-<h1 >Registration Form</h1>
-	<form method="post" action="process.php">
+<h1>Registration Form</h1>
+	<form method="post" action="form_process.php">
 	<table>
 	<tr>
 		<td>Name:</td>
 		<td><input type="text" name="txt1" required/></td>
 	</tr>
-
-    <tr>
+	<tr>
 		<td>Age:</td>
 		<td> <input type="Number" name="txt2" min="18" max="100" required/></td>
 	</tr> 
-	
-    <tr>
+	 
+	 <tr>
 		<td>Email:</td>
-		<td> <input type="Email" name="txt3" required/></td>
+		<td> <input type="text" name="txt2" required/></td>
 	</tr>
 	
 	<tr>
 		<td>Branch:</td>
-		<td> <input type="text" name="txt4" required/></td>
+		<td> <input type="text" name="txt3" required/></td>
 	</tr>
-
-    <tr>
+	
+	<tr>
 		<td>Select Time:</td>	
 		<td><input type="time" name="txt5"></td>
 	</tr>
-	
+
 	<tr>
 		<td>Password:</td>
-		<td> <input type="password" name="txt6" required/></td>
+		<td> <input type="password" name="txt4" required/></td>
 	</tr>
 	
 	<tr>
-	<td><input type="submit"/></td>
-	</tr>
+	<td>
+	<input type="submit"/></td>
+		</tr>
 	
+		
 	</table>
 	</form>
 	</body>
