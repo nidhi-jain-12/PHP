0d64e7f1d0ca12a4ee37c4696275c99352ee7248
nidhi-jain-12 <85149221+nidhi-jain-12@users.noreply.github.com>
2021-06-07 21:14:07 +0530

    Add files via upload

---
+++ day_8/delete.php
@@ -0,0 +1,18 @@
+<?php
+$host="localhost";
+$username="root";
+$password="";
+$dbname="database_1";
+
+// Connection Function
+$connection=mysqli_connect($host,$username,$password,$dbname);
+
+$id=$_GET['deleteid'];
+
+$q=mysqli_query($connection,"update tbl_user set is_delete=1 where user_id='{$id}' ")
+or die(mysqli_error($connection));
+
+if($q){
+    echo"<script>alert('Record deleted');window.location='tbl3.php';</script>";
+ }
+?>
